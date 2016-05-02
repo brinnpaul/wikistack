5f137493d0cb264d96014e7a5bba8d1a29db809e
Brinn Riordan <brinn.riordan@gmail.com>
2016-05-02 14:39:54 -0400

    wikistack outline

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var morgan = require('morgan');
+var router = require('./routes');
+var models = require('./models');
+var swig = require('swig');
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+// var pg = require('pg');
+// var conString = 'postgres://localhost:5432/wikistack';
+// var client = new pg.Client(conString);
+// client.connect();
+
+var app = express();
+
+
+app.set('views', path.join(__dirname, '/views')); // where to find the views
+app.set('view engine', 'html'); // what file extension do our templates have
+app.engine('html', swig.renderFile); // how to render html templates
+swig.setDefaults({ cache: false });
+
+// logging middleware
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//------------- database config --------------
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    server.listen(3001, function () {
+        console.log('Server is listening on port 3001!');
+    });
+})
+.catch(console.error);
+
+
+// // start the server
+// var server = app.listen(1337, function(){
+//   console.log('listening on port 1337');
+// });
---
+++ models/index.js
@@ -0,0 +1,35 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack', 'bpr', 'sunshine');
+
+
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING
+    },
+    urlTitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.STRING
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+        type: Sequelize.DATE
+    }
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
